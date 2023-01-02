f5312e365babf468ec809af7d4cc7b533bf5ba80
Leniza Souza <lenizarb@gmail.com>
2023-01-02 15:13:25 -0300

    finalizando o game

--- src/main/Calculations.java
+++ src/main/Calculations.java
@@ -12,22 +12,18 @@ public class Calculations {
 	
 	public Calculations(int nivel) {
 		Random random = new Random();
-		this.operacao = random.nextInt(4); //0-somar, 1-subtrair, 2-multiplicar, 3-dividir
+		this.operacao = random.nextInt(2); //0-somar, 1-subtrair, 2-multiplicar, 3-dividir
 		this.nivel = nivel;
 		
 		if(nivel == 1) {
 			//easy
 			this.valor1 = random.nextInt(10);
 			this.valor2 = random.nextInt(10);
-		}else if(nivel == 2) {
-			//medium
-			this.valor1 = random.nextInt(100);
-			this.valor2 = random.nextInt(100);
 		}else {
 			//hard
-			this.valor1 = random.nextInt(200);
-			this.valor2 = random.nextInt(200);
-		}			
+			this.valor1 = random.nextInt(100);
+			this.valor2 = random.nextInt(100);
+		}
 	}	
 	
 	public int getNivel() {
@@ -67,11 +63,65 @@ public class Calculations {
 			return "valor 1: " + this.getValor1()
 					+ "\nValor 2: " + this.getValor2()
 					+"\nOperacao: " + opcao
-					+"\nDificuldade: " + this.getNivel();
-			
-		
-		
+					+"\nDificuldade: " + this.getNivel();		
 	}
 	
-
+	public boolean somar(int valor) {
+		this.resultado = this.getValor1() + this.getValor2();
+		boolean acerto = false;
+		
+		if(valor == this.getResultado()) {
+			System.out.println("Resposta correta!");
+			acerto = true;
+		}else {
+			System.out.println("Estude mais!");
+		}
+		System.out.println(this.getValor1() + " + " + this.getValor2() + "=" + this.getResultado());
+		return acerto;		
+	}
+	
+	public boolean subtrair (int valor) {
+		this.resultado = this.getValor1() - this.getValor2();
+		boolean certo = false;
+		
+		if(valor == this.getResultado()) {
+			System.out.println("Resposta correta!");
+			certo = true;
+		}else {
+			System.out.println("Estude mais!");
+		}
+		System.out.println(this.getValor1() + " - " + this.getValor2() + "=" + this.getResultado());
+		return certo;		
+	}
+	public boolean multiplicar (int valor) {
+		this.resultado = this.getValor1() * this.getValor2();
+		boolean acerto = false;
+		
+		if(valor == this.getResultado()) {
+			System.out.println("Resposta correta!");
+			acerto = true;
+		}else {
+			System.out.println("Estude mais!");
+		}
+		System.out.println(this.getValor1() + " * " + this.getValor2() + "=" + this.getResultado());
+		return acerto;		
+	}
+	
+	public boolean dividir (int valor) {
+		this.resultado = this.getValor1() / this.getValor2();
+		boolean certo = false;
+		
+		if(valor == this.getResultado()) {
+			System.out.println("Resposta correta!");
+			certo = true;
+		}else {
+			System.out.println("Estude mais!");
+		}
+		System.out.println(this.getValor1() + " / " + this.getValor2() + "=" + this.getResultado());
+		return certo;		
+	}
+	
 }
+	
+	
+
--- src/view/Play.java
+++ src/view/Play.java
@@ -1,5 +1,80 @@
 package view;
 
-public class Play {
+import java.util.Scanner;
 
+import main.Calculations;
+
+public class Play {
+	
+	static Scanner input = new Scanner (System.in);
+	static int pontos = 0;
+	static Calculations calculos;
+	
+	public static void main (String[] args) {
+		Play.play();
+	}
+	
+	public static void play() {
+		System.out.println("Informe o nivel de dificuldade desejado[1 ou 2]: ");
+		int nivel = Play.input.nextInt();
+		
+		Play.calculos = new Calculations(nivel);
+		System.out.println("Informe o resultado para a seguinte operação: ");
+		
+		//soma
+		if(calculos.getOperacao() == 0) {
+			System.out.println(calculos.getValor1() + "+" + calculos.getValor2());
+		int resposta = Play.input.nextInt();
+		
+		if(calculos.somar(resposta)) {
+			Play.pontos += 1;
+			System.out.println("Você tem " + Play.pontos + "ponto(s).");
+			}
+		}
+		//subtrair
+				if(calculos.getOperacao() == 0) {
+					System.out.println(calculos.getValor1() + "-" + calculos.getValor2());
+				int resposta = Play.input.nextInt();
+				
+				if(calculos.subtrair(resposta)) {
+					Play.pontos += 1;
+					System.out.println("Você tem " + Play.pontos + "ponto(s).");
+					}
+				}
+		//multiplicar
+				if(calculos.getOperacao() == 0) {
+					System.out.println(calculos.getValor1() + "*" + calculos.getValor2());
+				int resposta = Play.input.nextInt();
+				
+				if(calculos.multiplicar(resposta)) {
+					Play.pontos += 1;
+					System.out.println("Você tem " + Play.pontos + "ponto(s).");
+					}
+				}
+		//dividir
+				if(calculos.getOperacao() == 0) {
+					System.out.println(calculos.getValor1() + "/" + calculos.getValor2());
+				int resposta = Play.input.nextInt();
+				
+				if(calculos.dividir(resposta)) {
+					Play.pontos += 1;
+					System.out.println("Você tem " + Play.pontos + "ponto(s).");
+					}
+				}else {
+					System.out.println("A operação " + calculos.getOperacao() + "não é reconhecida.");
+					
+				}
+				
+				System.out.println("Deseja continuar? [1-sim, 0-não]");
+				int continuar = Play.input.nextInt();
+				
+				if(continuar == 1){
+					Play.play();
+		}else {
+			System.out.println("Você fez " + Play.pontos + "ponto(s).");
+			System.out.println("Até a próxima!");
+			System.exit(0);
+			
+		}
+	}
 }
